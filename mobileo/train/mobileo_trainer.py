e15bd99a99c9ca2b27f9d6ed9cac72dec08f0ff9
DipanBartaula <deepanzcreed001@gmail.com>
2026-05-26 10:52:43 +0545

    transformer version error solved

--- mobileo/train/mobileo_trainer.py
+++ mobileo/train/mobileo_trainer.py
@@ -156,12 +156,13 @@ class mobileoTrainer(Trainer):
             kwargs["processing_class"] = kwargs.pop("tokenizer")
         super().__init__(*args, **kwargs)
 
-    def _get_train_sampler(self) -> Optional[torch.utils.data.Sampler]:
-        if self.train_dataset is None or not has_length(self.train_dataset):
+    def _get_train_sampler(self, train_dataset=None) -> Optional[torch.utils.data.Sampler]:
+        dataset = train_dataset if train_dataset is not None else self.train_dataset
+        if dataset is None or not has_length(dataset):
             return None
 
         if self.args.group_by_modality_length:
-            lengths = self.train_dataset.modality_lengths
+            lengths = dataset.modality_lengths
             return LengthGroupedSampler(
                 self.args.train_batch_size,
                 world_size=self.args.world_size * self.args.gradient_accumulation_steps,
